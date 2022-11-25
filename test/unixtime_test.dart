34145e7abaf4a5fc747880c8eba2c5916c8cf5a0
Kazuki Hamasaki <kazuki-h@iij.ad.jp>
2022-11-26 00:11:17 +0900

    fix test for local timezone

--- test/unixtime_test.dart
+++ test/unixtime_test.dart
@@ -1,15 +1,18 @@
 import 'package:unixtime/unixtime.dart';
 import 'package:test/test.dart';
 
+import 'package:timezone/timezone.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
 
 void main() {
+  late Location tokyo;
+  final unixepoch = DateTime.utc(1970, 1, 1, 0, 0, 0);
+
   setUp(() {
     tz.initializeTimeZones();
+    tokyo = tz.getLocation('Asia/Tokyo');
   });
-
-  final unixepoch = DateTime.utc(1970, 1, 1, 0, 0, 0);
 
   group('DateTime', () {
     test('can be get from unix epoch', () {
@@ -17,7 +20,6 @@ void main() {
     });
 
     test('can be get from local time', () {
-      final tokyo = tz.getLocation('Asia/Tokyo');
       final expected = tz.TZDateTime(tokyo, 2022, 11, 25, 16, 24, 55);
       final actual = 1669361095.toUnixTime();
       expect(actual.isAtSameMomentAs(expected), true);
@@ -26,7 +28,7 @@ void main() {
 
   group('Unix timestamp', () {
     test('can be get from DateTime', () {
-      final datetime = DateTime(2022, 11, 25, 16, 24, 55);
+      final datetime = tz.TZDateTime(tokyo, 2022, 11, 25, 16, 24, 55);
       expect(datetime.unixtime, 1669361095);
     });
   });
